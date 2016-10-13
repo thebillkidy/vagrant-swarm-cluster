2bd776bb96402745dfb7f48090da9638c3f47a7f
xavier geerinck <thebillkidy@hotmail.com>
2016-10-13 11:51:40 +0200

    Update destroy_cluster.sh

--- destroy_cluster.sh
+++ destroy_cluster.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
 echo "Destroying Consul containers..."
-docker -H 10.0.7.11:2375 rm -f consul1
-docker -H 10.0.7.12:2375 rm -f consul2
-docker -H 10.0.7.13:2375 rm -f consul3
+sudo docker -H 10.0.7.11:2375 rm -f consul1
+sudo docker -H 10.0.7.12:2375 rm -f consul2
+sudo docker -H 10.0.7.13:2375 rm -f consul3
 
 echo "Destroying Swarm Manager..."
-docker -H 10.0.7.11:2375 rm -f swarm_manager_00
+sudo docker -H 10.0.7.11:2375 rm -f swarm_manager_00
 
 # Run the first and second Swarm nodes
 echo "Destroying Swarm Agent #1..."
-docker -H 10.0.7.12:2375 rm -f swarm_agent_00
+sudo docker -H 10.0.7.12:2375 rm -f swarm_agent_00
 echo "Destroying Swarm Agent #2..."
-docker -H 10.0.7.13:2375 rm -f swarm_agent_01
+sudo docker -H 10.0.7.13:2375 rm -f swarm_agent_01
