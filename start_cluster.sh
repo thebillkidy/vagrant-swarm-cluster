979275ac99f640d2e7519c6a887602b4a07f1834
xavier geerinck <thebillkidy@hotmail.com>
2016-10-13 14:39:12 +0200

    Update start_cluster.sh

--- start_cluster.sh
+++ start_cluster.sh
@@ -1,34 +1,36 @@
 #!/bin/bash
-
-SWARM_VERSION=1.2.0
 MANAGER0_IP=10.0.7.11
-CONSUL0_IP=10.0.7.11
 NODE0_IP=10.0.7.12
 NODE1_IP=10.0.7.13
 
-# Run Consul on every Host (Consul can thus be running as master on any of these nodes!)
-#  $ docker run -d -p 8500:8500 --name=consul progrium/consul -server -bootstrap
-echo "Starting Consul containers..."
-sudo docker -H ${CONSUL0_IP}:2375 run -d -p 8500:8500 --name=consul progrium/consul -server -bootstrap
-#sudo docker -H ${CONSUL0_IP}:2375 run -d --restart always --name consul1 --net host consul agent -server -bind ${CONSUL0_IP} -client 10.0.7.11 -retry-join 10.0.7.11 -retry-join 10.0.7.12 -retry-join 10.0.7.13 -bootstrap-expect 3
+# Create manager and get the token
+vagrant ssh swarm_manager_00 -c "docker swarm init --advertise-addr ${MANAGER0_IP}"
+SWARM_TOKEN=`vagrant ssh swarm_manager_00 -c "sudo docker swarm join-token -q worker --quiet"`
 
-# Run a Swarm manager node (HA enabled)
-# $ docker run -d -p 4000:4000 swarm manage -H :4000 --replication --advertise <manager0_ip>:4000 consul://<consul0_ip>:8500
-echo "Starting Swarm Manager..."
-sudo docker -H ${MANAGER0_IP}:2375 run -d --restart always -p 4000:4000 --name swarm_manager_00 swarm:${SWARM_VERSION} manage -H :4000 --replication --advertise ${MANAGER0_IP}:2375 consul://${CONSUL0_IP}:8500
+# Add the nodes to this manager
+vagrant ssh swarm_agent_00 -c "docker swarm join --token ${SWARM_TOKEN} ${MANAGER0_IP}:2377"
+vagrant ssh swarm_agent_01 -c "docker swarm join --token ${SWARM_TOKEN} ${MANAGER0_IP}:2377"
 
-# Run the first and second Swarm nodes
-# $ docker run -d swarm join --advertise=<node_ip>:2375 consul://<consul0_ip>:8500
-echo "Starting Swarm Agent #1..."
-sudo docker -H ${NODE0_IP}:2375 run -d --restart always --name swarm_agent_00 swarm:${SWARM_VERSION} join --heartbeat 20s --ttl 30s --advertise ${NODE0_IP}:2375 consul://${CONSUL0_IP}:8500
-echo "Starting Swarm Agent #2..."
-sudo docker -H ${NODE1_IP}:2375 run -d --restart always --name swarm_agent_01 swarm:${SWARM_VERSION} join --heartbeat 20s --ttl 30s --advertise ${NODE1_IP}:2375 consul://${CONSUL0_IP}:8500
+# Create a network for monitoring tools
+vagrant ssh swarm_manager_00 -c "sudo docker network create monitoring -d overlay"
 
-echo "Waiting for the nodes to join the cluster..."
+# Start CAdvisor in global mode (an instance on every container) and forward 8080 so we can access the web UI if needed
+sudo docker service create --network=monitoring --mode global --name cadvisor \
+  --mount type=bind,source=/,target=/rootfs,readonly=true \
+  --mount type=bind,source=/var/run,target=/var/run,readonly=false \
+  --mount type=bind,source=/sys,target=/sys,readonly=true \
+  --mount type=bind,source=/var/lib/docker/,target=/var/lib/docker,readonly=true \
+  --publish 8080:8080 \
+  google/cadvisor:latest
+# TODO: Change this to kafka
+#  -storage_driver=elasticsearch \
+#  -storage_driver_es_host="http://elasticsearch:9200"
+
 # Wait for the nodes to join the cluster
-sleep 20
+echo "Waiting for the nodes to join the cluster..."
+sleep 5
 
 # Swarm cluster status
-export DOCKER_HOST_OLD=`echo $DOCKER_HOST`
-export DOCKER_HOST=tcp://${MANAGER0_IP}:4000
-sudo docker -H ${MANAGER0_IP}:4000 info
+#export DOCKER_HOST_OLD=`echo $DOCKER_HOST`
+#export DOCKER_HOST=tcp://${MANAGER0_IP}:4000
+#sudo docker -H ${MANAGER0_IP}:4000 info
