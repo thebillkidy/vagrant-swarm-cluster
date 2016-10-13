7419e9b12380e5904b4b67bf0385617930cd175c
xavier geerinck <thebillkidy@hotmail.com>
2016-10-13 12:48:12 +0200

    Update start_cluster.sh

--- start_cluster.sh
+++ start_cluster.sh
@@ -15,7 +15,7 @@ sudo docker -H ${CONSUL0_IP}:2375 run -d -p 8500:8500 --name=consul progrium/con
 # Run a Swarm manager node (HA enabled)
 # $ docker run -d -p 4000:4000 swarm manage -H :4000 --replication --advertise <manager0_ip>:4000 consul://<consul0_ip>:8500
 echo "Starting Swarm Manager..."
-sudo docker -H ${MANAGER0_IP}:2375 run -d --restart always -p 4000:4000 --name swarm_manager_01 swarm:${SWARM_VERSION} manage -H :4000 --replication --advertise ${MANAGER0_IP}:2375 consul://${CONSUL0_IP}:8500
+sudo docker -H ${MANAGER0_IP}:2375 run -d --restart always -p 4000:4000 --name swarm_manager_00 swarm:${SWARM_VERSION} manage -H :4000 --replication --advertise ${MANAGER0_IP}:2375 consul://${CONSUL0_IP}:8500
 
 # Run the first and second Swarm nodes
 # $ docker run -d swarm join --advertise=<node_ip>:2375 consul://<consul0_ip>:8500
